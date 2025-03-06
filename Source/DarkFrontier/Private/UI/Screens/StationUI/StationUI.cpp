28bcdbede1fbbffb54ed5b0eaf094d7e78f82180
Phantom-101 <ccradical2468@gmail.com>
2025-03-06 01:27:34 -0800

    station ui and trade ui scaffold

---
+++ Source/DarkFrontier/Private/UI/Screens/StationUI/StationUI.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UI/Screens/StationUI/StationUI.h"
+#include "CommonButtonBase.h"
+#include "Structures/StructureController.h"
+#include "Structures/StructureLocation.h"
+
+void UStationUI::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	UndockButton->OnClicked().Clear();
+	UndockButton->OnClicked().AddUObject<UStationUI>(this, &UStationUI::HandleUndock);
+}
+
+void UStationUI::NativeOnActivated()
+{
+	Super::NativeOnActivated();
+
+	GetDesiredFocusTarget()->SetFocus();
+}
+
+UWidget* UStationUI::NativeGetDesiredFocusTarget() const
+{
+	return UndockButton;
+}
+
+void UStationUI::HandleUndock() const
+{
+	const AStructureController* Controller = Cast<AStructureController>(GetWorld()->GetFirstPlayerController());
+	const AStructure* Player = Cast<AStructure>(Controller->GetPawn());
+	Player->GetLocation()->ExitDock();
+}
---
+++ Source/DarkFrontier/Public/UI/Screens/StationUI/StationUI.h
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "StationUI.generated.h"
+
+class UCommonButtonBase;
+
+/**
+ * 
+ */
+UCLASS()
+class DARKFRONTIER_API UStationUI : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> ShipButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> TradeButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> RepairButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> UpgradeButton;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> PassengersButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> UndockButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> StationButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> HangarButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> BuyShipButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> TravelButton;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> MissionsButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> ExitShipButton;
+
+	virtual void NativeConstruct() override;
+
+	virtual void NativeOnActivated() override;
+
+	virtual UWidget* NativeGetDesiredFocusTarget() const override;
+
+private:
+
+	void HandleUndock() const;
+	
+};
