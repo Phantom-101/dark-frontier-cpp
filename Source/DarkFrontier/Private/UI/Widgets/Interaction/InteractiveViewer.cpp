2e907eb4de877c60325bbeb090382596a25450e4
Phantom-101 <ccradical2468@gmail.com>
2025-12-19 00:21:23 -0800

    interactive viewer widget

---
+++ Source/DarkFrontier/Private/UI/Widgets/Interaction/InteractiveViewer.cpp
@@ -0,0 +1,87 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UI/Widgets/Interaction/InteractiveViewer.h"
+#include "CommonButtonBase.h"
+#include "Macros.h"
+#include "Blueprint/DragDropOperation.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Components/NamedSlot.h"
+
+void UInteractiveViewer::NativeConstruct()
+{
+	Super::NativeConstruct();
+	ResetButton->OnClicked().AddUObject(this, &UInteractiveViewer::Reset);
+	ZoomInButton->OnClicked().AddUObject(this, &UInteractiveViewer::ZoomIn);
+	ZoomOutButton->OnClicked().AddUObject(this, &UInteractiveViewer::ZoomOut);
+	UpdateZoom(FVector2D::ZeroVector);
+}
+
+void UInteractiveViewer::Reset()
+{
+	ZoomLevel = 0;
+	UpdateZoom(FVector2D::ZeroVector);
+	
+	UCanvasPanelSlot* PanelSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(ContentSlot);
+	PanelSlot->SetPosition(FVector2D::ZeroVector);
+}
+
+void UInteractiveViewer::ZoomIn()
+{
+	const FVector2D Center = GetCenter();
+	ZoomLevel = FMath::Clamp(ZoomLevel + 1, MinZoomLevel, MaxZoomLevel);
+	UpdateZoom(Center);
+}
+
+void UInteractiveViewer::ZoomOut()
+{
+	const FVector2D Center = GetCenter();
+	ZoomLevel = FMath::Clamp(ZoomLevel - 1, MinZoomLevel, MaxZoomLevel);
+	UpdateZoom(Center);
+}
+
+FReply UInteractiveViewer::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+	return FReply::Handled().DetectDrag(TakeWidget(), EKeys::LeftMouseButton);
+}
+
+void UInteractiveViewer::NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation)
+{
+	Super::NativeOnDragDetected(InGeometry, InMouseEvent, OutOperation);
+	OutOperation = NewObject<UPanOperation>();
+	OutOperation->OnDragged.AddDynamic(this, &UInteractiveViewer::HandlePan);
+}
+
+FVector2D UInteractiveViewer::GetCenter() const
+{
+	const UCanvasPanelSlot* PanelSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(ContentSlot);
+	return PanelSlot->GetPosition() / GetScaleFactor();
+}
+
+void UInteractiveViewer::UpdateZoom(const FVector2D& Focal) const
+{
+	ContentSlot->SetRenderScale(FVector2D(GetScaleFactor()));
+
+	UCanvasPanelSlot* PanelSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(ContentSlot);
+	PanelSlot->SetPosition(Focal * GetScaleFactor());
+}
+
+float UInteractiveViewer::GetScaleFactor() const
+{
+	return FMath::Pow(ZoomFactor, ZoomLevel);
+}
+
+void UInteractiveViewer::HandlePan(UDragDropOperation* Operation)
+{
+	const UPanOperation* Pan = Cast<UPanOperation>(Operation);
+	GUARD(IsValid(Pan));
+	UCanvasPanelSlot* PanelSlot = UWidgetLayoutLibrary::SlotAsCanvasSlot(ContentSlot);
+	PanelSlot->SetPosition(PanelSlot->GetPosition() + Pan->CursorDelta);
+}
+
+void UPanOperation::Dragged_Implementation(const FPointerEvent& PointerEvent)
+{
+	CursorDelta = PointerEvent.GetCursorDelta();
+	Super::Dragged_Implementation(PointerEvent);
+}
---
+++ Source/DarkFrontier/Public/UI/Widgets/Interaction/InteractiveViewer.h
@@ -0,0 +1,92 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "Blueprint/DragDropOperation.h"
+#include "InteractiveViewer.generated.h"
+
+class UCommonButtonBase;
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class DARKFRONTIER_API UInteractiveViewer : public UCommonUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> ResetButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> ZoomInButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UCommonButtonBase> ZoomOutButton;
+
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UNamedSlot> ContentSlot;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	float ZoomFactor = 1.5;
+
+	UPROPERTY(BlueprintReadOnly, VisibleInstanceOnly)
+	int ZoomLevel = 0;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	int MaxZoomLevel = 5;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	int MinZoomLevel = -5;
+
+	virtual void NativeConstruct() override;
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void Reset();
+
+	UFUNCTION(BlueprintCallable)
+	void ZoomIn();
+
+	UFUNCTION(BlueprintCallable)
+	void ZoomOut();
+
+protected:
+
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+
+	virtual void NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation) override;
+
+private:
+
+	FVector2D GetCenter() const;
+
+	void UpdateZoom(const FVector2D& Focal) const;
+
+	float GetScaleFactor() const;
+
+	UFUNCTION()
+	void HandlePan(UDragDropOperation* Operation);
+	
+};
+
+UCLASS()
+class DARKFRONTIER_API UPanOperation : public UDragDropOperation
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadOnly, VisibleInstanceOnly)
+	FVector2D CursorDelta;
+
+protected:
+
+	virtual void Dragged_Implementation(const FPointerEvent& PointerEvent) override;
+	
+};
